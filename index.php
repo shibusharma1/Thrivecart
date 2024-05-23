0db6f86b5370287e2925e6a65e2fb9fffff04460
shibusharma1 <shibusharma807@gmail.com>
2024-05-23 14:01:42 +0545

    Password column added

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ if(!isset($_SESSION['uid'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dash</title>
+    <title>Dashboard</title>
 </head>
 <body>
         <h1>Welcome to Dashboard,
